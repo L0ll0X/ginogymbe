08c0ef0c74c4f84f559a998137b5318240369fca
Matteo Scalese <matteoscales@gmail.com>
2025-10-13 11:26:27 +0200

    add lombok

--- ginogymbe/src/main/java/com/example/business/GruppoMuscolare.java
+++ ginogymbe/src/main/java/com/example/business/GruppoMuscolare.java
@@ -1,5 +1,13 @@
 package com.example.business;
 
-public interface GruppoMuscolare {
+import java.util.List;
 
+import com.example.data.GruppoMuscolareEntity;
+
+public interface GruppoMuscolare  {
+
+	public List<GruppoMuscolareEntity> getAll();
+	GruppoMuscolareEntity findById(Long id);
+	GruppoMuscolareEntity save(GruppoMuscolareEntity entity);
+	void delete(Long id);
 }
