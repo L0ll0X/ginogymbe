bff3db617166c05a2ffa569f2c36bbdbbdf2abf7
Martina Pascucci <marty.pascucci@gmail.com>
2025-10-17 12:04:47 +0200

    update

--- ginogymbe/src/main/java/com/ginogym/business/DTOs/MachineDTO.java
+++ ginogymbe/src/main/java/com/ginogym/business/DTOs/MachineDTO.java
@@ -11,5 +11,5 @@ public class MachineDTO {
     private Long id;
     private String name;
     private String description;
-   private MuscleGroupDTO muscleGroup;
+    private MuscleGroupDTO muscleGroup;
 }
