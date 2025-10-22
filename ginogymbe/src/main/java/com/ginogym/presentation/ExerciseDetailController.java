c30afdb6a8e0bf552faa30fb633aa8b8ef24e87c
Martina Pascucci <marty.pascucci@gmail.com>
2025-10-22 09:51:24 +0200

    update exercise and exercise detail

--- ginogymbe/src/main/java/com/ginogym/presentation/ExerciseDetailController.java
+++ ginogymbe/src/main/java/com/ginogym/presentation/ExerciseDetailController.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/exerciseDetails")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class ExerciseDetailController {
 
